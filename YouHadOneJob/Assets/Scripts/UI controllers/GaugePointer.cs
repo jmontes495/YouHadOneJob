245e3f12f5723d38317da7098a01bad01c34097f
Nicolas Trillos <ntrillos@jamcity.com>
2019-08-03 20:49:05 -0500

    Added controlles for gauge and Painter

---
+++ YouHadOneJob/Assets/Scripts/UI controllers/GaugePointer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace YouHadOneJob
+{
+    public class GaugePointer : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 100f)]
+        private float sanity;
+        [SerializeField]
+        private float minRotationAngle;
+        [SerializeField]
+        private float maxRotationAngle;
+        [SerializeField]
+        private GameObject pointer;
+
+        private float mySanity;
+
+        private void Start()
+        {
+            sanity = 100.0f;
+            mySanity = 100.0f;
+        }
+
+        private void Update()
+        {
+            if(sanity != mySanity)
+            {
+                mySanity = sanity;
+                float rotation = CalculateRotation();
+                Vector3 rotationVector = transform.rotation.eulerAngles;
+                rotationVector.z = rotation;
+                pointer.transform.rotation = Quaternion.Euler(rotationVector);
+            }
+        }
+
+        private float CalculateRotation()
+        {
+            float rotation = 0.0f;
+            if (mySanity.Equals(50.0f))
+                return rotation;
+
+            if (mySanity < 50.0f)
+                rotation = Mathf.Clamp((minRotationAngle * (50.0f - mySanity)) / 50.0f, 0.0f, minRotationAngle);
+            if (mySanity > 50.0f)
+                rotation = Mathf.Clamp((maxRotationAngle * (mySanity - 50.0f)) / 50.0f, maxRotationAngle, 0.0f);
+
+            return rotation;
+        }
+
+        public void SetSanity(float san)
+        {
+            sanity = san;
+        }
+    }
+}
---
+++ YouHadOneJob/Assets/Scripts/UI controllers/PaintStep.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace YouHadOneJob
+{
+    public class PaintStep : MonoBehaviour
+    {
+        [SerializeField, Range(0, 13)]
+        private int step;
+        [SerializeField]
+        private SpriteRenderer painter;
+        [SerializeField]
+        private Sprite[] spriteCollection;
+
+        private int currentStep;
+
+        private void Start()
+        {
+            step = 0;
+            currentStep = 0;
+            painter.sprite = spriteCollection[currentStep];
+        }
+
+        private void Update()
+        {
+            if (currentStep != step)
+            {
+                currentStep = step;
+                painter.sprite = spriteCollection[currentStep];
+            }
+        }
+        
+        public void SetStep(int stp)
+        {
+            step = stp;
+        }
+    }
+}
