ae700066d7b5efab78fb05554e2e93d69c4955e3
Raul Blanco <rfblanco93@gmail.com>
2019-08-04 12:39:04 -0500

    Load main screen

---
+++ YouHadOneJob/Assets/Scripts/UI controllers/EntryButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EntryButton : MonoBehaviour
+{
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+}
